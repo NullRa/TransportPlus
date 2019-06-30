f417b7aa40d132f52fccb3ec18e13c7615f3d41b
Shao-Jie Tang <s510248@livemail.tw>
2019-06-30 11:39:47 +0800

    Add alert dialog to handle exceptions

--- Common/ErrorCode.swift
+++ Common/ErrorCode.swift
@@ -1,15 +1,28 @@
-//
-//  Error.swift
-//  Transportation_PLUS
-//
-//  Created by Jay on 2019/6/26.
-//  Copyright © 2019 Null. All rights reserved.
-//
-
 import Foundation
 
 enum ErrorCode: Error {
     case UrlError
     case DataError
     case JsonDecodeError
+    case CoreDataError
+    var alertTitle:String{
+        var result = ""
+        switch self {
+        case .UrlError: result = "UrlError"
+        case .DataError: result = "DataError"
+        case .JsonDecodeError: result = "JsonDecodeError"
+        case .CoreDataError: result = "CoreDataError"
+        }
+        return result
+    }
+    var alertMessage:String{
+        var result = ""
+        switch self {
+        case .UrlError: result = "UrlError"
+        case .DataError: result = "DataError"
+        case .JsonDecodeError: result = "JsonDecodeError"
+        case .CoreDataError: result = "CoreDataError"
+        }
+        return result
+    }
 }
