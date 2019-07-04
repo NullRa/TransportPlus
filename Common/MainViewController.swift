b0c4ded092486aadcf0d1dfa72a3ce28d57f605e
Shao-Jie Tang <s510248@livemail.tw>
2019-07-04 09:55:52 +0800

    Fix bug: location authority

--- Common/MainViewController.swift
+++ Common/MainViewController.swift
@@ -1,35 +1,45 @@
 import UIKit
 import CoreData
+import CoreLocation
 
 class MainViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
-    
-    
-   
-   
-    
-    
-    
-    //    func cleanUpProducts() {
-    //        let request = NSFetchRequest(entityName: "Product")
-    //
-    //        do {
-    //            let results = try moc.executeFetchRequest(request) as! [Product]
-    //
-    //            for result in results {
-    //                moc.deleteObject(result)
-    //            }
-    //
-    //            do {
-    //                try moc.save()
-    //            }catch{
-    //                fatalError("Failure to save context: \(error)")
-    //            }
-    //        }catch{
-    //            fatalError("Failed to fetch data: \(error)")
-    //        }
-    //    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        // 首次使用 向使用者詢問定位自身位置權限
+        if CLLocationManager.authorizationStatus()
+            == .notDetermined {
+            // 取得定位服務授權
+            MapManager.shared.manager.requestWhenInUseAuthorization()
+            
+            // 開始定位自身位置
+            MapManager.shared.manager.startUpdatingLocation()
+        }
+            // 使用者已經拒絕定位自身位置權限
+        else if CLLocationManager.authorizationStatus()
+            == .denied {
+            // 提示可至[設定]中開啟權限
+            let alertController = UIAlertController(
+                title: "定位權限已關閉",
+                message:
+                "如要變更權限，請至 設定 > 隱私權 > 定位服務 開啟",
+                preferredStyle: .alert)
+            let okAction = UIAlertAction(
+                title: "確認", style: .default, handler:nil)
+            alertController.addAction(okAction)
+            self.present(
+                alertController,
+                animated: true, completion: nil)
+        }
+            // 使用者已經同意定位自身位置權限
+        else if CLLocationManager.authorizationStatus()
+            == .authorizedWhenInUse {
+            // 開始定位自身位置
+            MapManager.shared.manager.startUpdatingLocation()
+        }
+    }
 }
