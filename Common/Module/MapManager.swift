ba8e62402c88b254f17608fe56720603e62c3fa4
Shao-Jie Tang <s510248@livemail.tw>
2019-07-07 10:09:03 +0800

    Refactor rename too short property Refactor rename too short property like lon:longitude, lat:latitude.

--- Common/Module/MapManager.swift
+++ Common/Module/MapManager.swift
@@ -20,8 +20,8 @@ class MapManager{
     
     func searchAction(searchText:String,completion: @escaping (CLLocationCoordinate2D?,Error?)->()) {
         let geoCoder = CLGeocoder()
-        var lon : Double = 0
-        var lat : Double = 0
+        var longitude : Double = 0
+        var latitude : Double = 0
         var center : CLLocationCoordinate2D?
         var funcError : Error? = nil
         
@@ -32,13 +32,13 @@ class MapManager{
             }
             if let tmpPlacemarks = placemarks,let location = tmpPlacemarks[0].location{
                 //placemark.location.coordinate 取得經緯度的參數
-                lon = location.coordinate.longitude
-                lat = location.coordinate.latitude
-                if(lat < 22 || lat > 27 || lon < 118 || lon > 122 && lat != 0 && lon != 0){
+                longitude = location.coordinate.longitude
+                latitude = location.coordinate.latitude
+                if(latitude < 22 || latitude > 27 || longitude < 118 || longitude > 122 && latitude != 0 && longitude != 0){
                     funcError = ErrorCode.AddressRangeError
                 }
             }
-            geoCoder.reverseGeocodeLocation(CLLocation(latitude: lat , longitude: lon), completionHandler: {
+            geoCoder.reverseGeocodeLocation(CLLocation(latitude: latitude , longitude: longitude), completionHandler: {
                 (placemarks:[CLPlacemark]?,error:Error?) in
                 UserDefaults.standard.set(["zh_TW"], forKey: "AppleLanguages")
                 if let _ = error, !(funcError is ErrorCode) {
@@ -49,7 +49,7 @@ class MapManager{
                     let resultAddress = subAdministrativeArea + locality + name
                     if resultAddress.contains(searchText){
                         //搜尋的字都在回傳的結果之中,限制搜尋經緯度範圍
-                        center = CLLocationCoordinate2D(latitude: lat, longitude: lon)
+                        center = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
                     }
                 }
                 completion(center,funcError)
